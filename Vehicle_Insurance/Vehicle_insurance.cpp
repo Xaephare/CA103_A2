f3ca14e82d9f636d2402bd9ec39212bce48d196b
trollheimsgrott <finlay.macbeth@gmail.com>
2022-06-15 14:43:18 +1200

    preparing to pull main into this branch

--- Vehicle_Insurance/Vehicle_insurance.cpp
+++ Vehicle_Insurance/Vehicle_insurance.cpp
@@ -2,15 +2,18 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <string.h>
+
 
 using std::string;
+using std::ifstream;
 using std::vector;
 using std::cin;
 using std::cout;
 using std::endl;
 
 struct vehicleInfo {
-	int id;
+	int id =0;
 	int insuredValue =0;
 	string registration = "null";
 	string make = "null";
@@ -22,11 +25,48 @@ struct vehicleInfo {
 
 };
 
+void read()
+{
+	ifstream fin{};
+	string line;
+	// Open an existing file
+	fin.open("report.csv");
+	while (!fin.eof()) {
+		fin >> line;
+		cout << line << " ";
+	}
+}
 
 vector<vehicleInfo> cars;
 
-void vehicleData() {
+//int fetchLastVehicleNum() {
+//
+//	std::ofstream file;
+//
+//	string numStr;
+//	std::fstream vehicleId;
+//	vehicleId.open("vehicle_id_counter.txt", std::ios::in);
+//	getline(vehicleId, numStr);
+//	int vehicleNum = std::stoi(numStr);
+//	vehicleId.close();
+//
+//	return vehicleNum;
+//}
+//
+//void writeTxt(string fileName, int vehicleNumber) {
+//	std::fstream file;
+//	file.open(fileName, std::ios::out);
+//	file << vehicleNumber;
+//	file.close();
+//}
 
+
+void vehicleData() { 
+	//vehicleInfo addVehic;
+
+	//int lastVehicNum = fetchLastVehicleNum();
+	//addVehic.id = ++lastVehicNum;
+	//writeTxt("data/vehicle_id_counter.txt", lastVehicNum);
 
 	vehicleInfo car = {};
 	cout << "Please enter the registration plate of vehicle: \n";
@@ -63,25 +103,28 @@ void printVehicle() {
 	};
 }
 
-bool writeCsv(string fileName, string registration, string make, string model, int year, string colour, int doors, string transmission) {
+
+void writeCsv(string fileName, int vehicleNum, string registration, string make, string model, int year, string colour, int doors, string transmission) {
 
 
-	std::ofstream file("\\Vehicle_Insurance\\data");
+	std::ofstream file;
 
 	file.open(fileName, std::ios::out | std::ios_base::app);
-	file<< "Registration Plate" << "," << registration << "," << "Make" << "," << make << "," << "Model" << "," << model << "," << "Year" << "," << year << "," << "Colour" << "," << colour << "," << "Doors" << "," << doors << "," << "Transmission" << "," << transmission << endl;
+	file << "ID" << "," << vehicleNum << "," << "Registration Plate" << "," << registration << "," << "Make" << "," << make << "," << "Model" << "," << model << "," << "Year" << "," << year << "," << "Colour" << "," << colour << "," << "Doors" << "," << doors << "," << "Transmission" << "," << transmission << endl;
 	file.close();
-
-	return true;
 };
 
 
 
 
+
+
+
 int main() {
+	read();
 	printVehicle();
 	vehicleData();
-	writeCsv("data/vehicleDb.csv", cars[0].registration, cars[0].make, cars[0].model, cars[0].year, cars[0].colour, cars[0].doors, cars[0].transmission);
+	writeCsv("data/vehicleDb.csv", cars[0].id, cars[0].registration, cars[0].make, cars[0].model, cars[0].year, cars[0].colour, cars[0].doors, cars[0].transmission);
 
 	
 	return 0;
